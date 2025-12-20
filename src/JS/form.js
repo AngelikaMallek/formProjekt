6d1e3fa91754d4035504dd3e5555cff360756411
Angelika Krefta <krefta.angelika@gmail.com>
2025-12-20 10:36:30 +0100

    add task list with delete option

--- src/JS/form.js
+++ src/JS/form.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#taskForm');
+const taskList = document.querySelector('#taskList');
+
 
 const showAlert = (msg) => alert(msg);
 const showConfirm = (msg) => confirm(msg);
@@ -59,9 +61,53 @@ form.addEventListener('submit', (e) => {
 
   if (!showConfirm('Are you sure you want to add this task?')) return;
 
-  const tag = showPrompt('Add a tag for this task (optional):');
+    const tag = showPrompt('Add a tag for this task (optional):');
 
-  showAlert('Task added successfully' + (tag ? ` with tag: ${tag}` : ''));
+  const task = {
+    topic: data.get('topic'),
+    description: data.get('description'),
+    date: data.get('date'),
+    priority: data.get('priority'),
+    status: data.get('status'),
+    assignedTo: data.get('assignedTo'),
+    repeat: data.get('repeat'),
+    tag: tag
+  };
+
+  addTaskToList(task);
+
+  showAlert('Task added successfully');
 
   form.reset();
+
 });
+
+const addTaskToList = (task) => {
+  const li = document.createElement('li');
+  li.classList.add('list__item');
+
+  li.innerHTML = `
+    <strong>Topic:</strong> ${task.topic}
+    <strong>Description:</strong>  ${task.description}
+    <strong>Date:</strong> ${task.date}
+    <strong>Priority:</strong> ${task.priority} 
+    <strong>Status:</strong> ${task.status}
+    <strong>Assigned to:</strong> ${task.assignedTo}
+    <strong>Repeat:</strong> ${task.repeat}
+    ${task.tag ? `<strong>Tag:</strong> ${task.tag}<br>` : '<br>'}
+    <button class="task__delete">Usuń</button>
+  `;
+
+  const deleteButton = li.querySelector('.task__delete');
+
+  deleteButton.addEventListener('click', () => {
+    if (confirm('Czy na pewno chcesz usunąć to zadanie?')) {
+      li.remove();
+    }
+  });
+
+  taskList.appendChild(li);
+};
+
+
+
